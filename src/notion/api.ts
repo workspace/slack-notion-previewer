d3d6c2c552e9540b7a4a5319a8c77ddd3f26da5f
Kimin Ryu <ryu@holix.com>
2022-02-06 17:04:14 +0900

    implement getPageByURL
    notion's url pattern: notion.so/{workspace}/{page slug with dash}-{page id without dash}
    
    1. getLastSegment from url
    2. split last segment with dash and assume that last element is page id to retrieve.

---
+++ src/notion/api.ts
@@ -0,0 +1,43 @@
+import { Client } from '@notionhq/client'
+import {
+    Page,
+} from "@notionhq/client/build/src/api-types"
+
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN
+})
+
+function getLastSegment(url: URL): string {
+    const segments = url.pathname.split("/")
+    const segment = segments.pop() || segments.pop()
+    if (segment) {
+        return segment
+    } else {
+        throw new Error("cannot get last segment from url")
+    }
+}
+
+function toPageId(lastSegment: string): string {
+    const elements = lastSegment.split("-")
+    const pageIdCandidate = elements.pop() || elements.pop()
+    if (pageIdCandidate) {
+        return pageIdCandidate
+    } else {
+        throw new Error("cannot get pageId from last segment")
+    }
+}
+
+function getPageIdByURL(url: URL): string {
+    return toPageId(getLastSegment(url))
+}
+export async function getPageByURL(pageUrl: URL): Promise<Page | undefined> {
+    try {
+        const pageId = getPageIdByURL(pageUrl);
+        const page = await notion.pages.retrieve({
+            page_id: pageId
+        });
+        return page;
+    } catch (error) {
+        console.error(error);
+    }
+}
